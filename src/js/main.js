7a3544bae227ea1a4d74af77a699e0e10bd969fa
despairpanta <xenia.mikhaleva@studentfr.ch>
2025-02-18 18:43:50 +0100

    finalisation

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var onionsChanges = document.querySelector(".productOnion");
 var jalapenoChanges = document.querySelector(".productJalapeno");
 var classicChanges = document.querySelector(".productClassic");
+var footerChanges = document.querySelector(".style-footer-title");
 
 function colorChangeOnion() {
   onionsChanges.classList.toggle("is-active");
@@ -11,7 +12,11 @@ function colorChangeJalapeno() {
 function colorChangeClassic() {
   classicChanges.classList.toggle("is-active");
 }
+function colorChangeTitle() {
+  footerChanges.classList.toggle("is-active");
+}
 
 onionsChanges.addEventListener("click", colorChangeOnion);
 jalapenoChanges.addEventListener("click", colorChangeJalapeno);
 classicChanges.addEventListener("click", colorChangeClassic);
+footerChanges.addEventListener("click", colorChangeTitle);
